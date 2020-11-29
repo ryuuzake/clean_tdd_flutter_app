7a84a74cbf42110dd3b6e8755d003f1a8b2676ae
Riza Nafis <riza.nafis@gmail.com>
2020-11-29 13:47:26 +0700

    domain layer refactoring

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,11 +1,13 @@
+import 'package:clean_tdd_app/core/usecases/usecase.dart';
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 import '../../../../core/error/failures.dart';
 import '../entities/number_trivia.dart';
 import '../repositories/number_trivia_repository.dart';
 
-class GetConcreteNumberTrivia {
+class GetConcreteNumberTrivia extends UseCase<NumberTrivia, Params> {
   final NumberTriviaRepository repository;
 
   GetConcreteNumberTrivia(this.repository);
@@ -15,4 +17,15 @@ class GetConcreteNumberTrivia {
   }) async {
     return await repository.getConcreteNumberTrivia(number);
   }
+
+  @override
+  Future<Either<Failure, NumberTrivia>> call(Params params) async {
+    return await repository.getConcreteNumberTrivia(params.number);
+  }
+}
+
+class Params extends Equatable {
+  final int number;
+
+  Params({@required this.number}) : super([number]);
 }
