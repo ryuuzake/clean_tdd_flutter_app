79381eee28ed5d7ab33d37417f3ab6a1a25c016f
Riza Nafis <riza.nafis@gmail.com>
2020-11-30 09:50:54 +0700

    update pubspec

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -3,7 +3,10 @@ import 'package:equatable/equatable.dart';
 abstract class Failure extends Equatable {
   // If the subclass have some properties, they'll get passed to this constructor
   // so that Equatable can perform value comparison.
-  Failure([List properties = const <dynamic>[]]) : super(properties);
+  const Failure();
+
+  @override
+  List<Object> get props => [];
 }
 
 // General failures.
